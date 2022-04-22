9257bf8ea60a0a18d2895043b28eff1c78241b83
Kevin Winkel <k.winkel@fio.de>
2022-04-22 16:25:24 +0200

    add first draft of module graylog_stream

---
+++ plugins/modules/graylog_stream.py
@@ -0,0 +1,171 @@
+#!/usr/bin/python
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+DOCUMENTATION = r'''
+---
+module: graylog_stream
+
+short_description: Module to create/update/delete Graylog streams
+
+version_added: "1.0.0"
+
+description: 
+  - Module ensures that the given streams exists on the target Graylog instance.
+
+options:
+  endpoint_url:
+    description: Graylog endpoint URL.
+    required: true
+    type: str
+  endpoint_token:
+    description: Token which will be used for the API requests.
+    required: true
+    type: str
+  validate_certs:
+    description: Validate certs for endpoint_url.
+    required: false
+    type: bool
+  state:
+    description: The desired state.
+    required: true
+    default: present
+    choices: [ "present", "absent" ]
+    type: str
+  name:
+    description: The name of the stream.
+    required: true
+    type: str
+
+notes:
+  - Does not require any additional dependencies.
+
+
+author:
+  - FIO SYSTEMS AG (@FIO-SYSTEMS-AG)
+'''
+
+EXAMPLES = r'''
+- name: Test with a message
+  fio.graylog.graylog_stream:
+    endpoint_url: http://localhost:9000
+    endpoint_token: foobar:token
+    validate_certs: false
+    state: present
+    name: myapp
+'''
+
+RETURN = r'''
+#
+'''
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.urls import fetch_url, to_text
+from typing import NoReturn
+import base64
+import json
+
+
+def run_module():
+  module_args = dict(
+    endpoint_url=dict(type='str', required=True),
+    endpoint_token=dict(type='str', required=True),
+    validate_certs=dict(type='bool', required=False),
+    state=dict(type='str', required=True),
+    name=dict(type='str', required=True),
+  )
+
+  result = dict(
+    changed=False
+  )
+
+  module = AnsibleModule(
+    argument_spec=module_args,
+    supports_check_mode=True
+  )
+
+  param_state = module.params["state"]
+  param_name = module.params["name"]
+
+  streams_response = get_streams(module)
+
+  # check
+  existing_stream = None
+  for item in streams_response['streams']:
+    if (item['title'] == param_name):
+      existing_stream = item  
+  
+  if module.check_mode:
+    result['changed'] = shouldCreateStream(param_state, existing_stream) or shouldUpdateStream(param_state, existing_stream) or shouldDeleteStream(param_state, existing_stream)
+    module.exit_json(**result)
+
+  # execute
+  if (shouldCreateStream(param_state, existing_stream)):
+    result['changed'] = create_stream(module)
+  elif (shouldUpdateStream(param_state, existing_stream)):
+    result['changed'] = update_stream(module, existing_stream)
+  elif (shouldDeleteStream(param_state, existing_stream)):
+    result['changed'] = delete_stream(module, existing_stream)
+
+  module.exit_json(**result)
+
+
+def shouldCreateStream(state: str, existing_stream: dict) -> bool:
+  return state == "present" and existing_stream is None
+
+
+def shouldUpdateStream(state: str, existing_stream: dict) -> bool:
+  return state == "present" and existing_stream is not None
+
+
+def shouldDeleteStream(state: str, existing_stream: dict) -> bool:
+  return state == "absent" and existing_stream is not None
+
+
+def get_streams(module: AnsibleModule) -> dict:
+  response, info = fetch_url(module=module, url=(get_apiBaseUrl(module) + "/api/streams"), headers=get_apiRequestHeaders(module), method='GET')
+
+  if info['status'] != 200:
+    module.fail_json(msg=info['msg'])
+  
+  return json.loads(to_text(response.read(), errors='surrogate_or_strict'))
+
+
+def create_stream(module: AnsibleModule) -> bool:
+  print('create stream')
+  return True
+
+
+def update_stream(module: AnsibleModule, existing_stream: dict) -> bool:
+  print('update stream')
+  return True
+
+
+def delete_stream(module: AnsibleModule, existing_stream: dict) -> bool:
+  print('delete stream')
+  return True
+
+
+def get_apiBaseUrl(module: AnsibleModule) -> str:
+  return module.params["endpoint_url"]
+
+
+def get_apiRequestHeaders(module: AnsibleModule) -> dict:
+  token = module.params["endpoint_token"] + ':token' 
+  token_bytes = token.encode('utf-8')
+  encoded_token = base64.b64encode(token_bytes).decode('utf-8')
+  
+  return { 
+    'Authorization': 'Basic ' + encoded_token,
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  }
+
+
+def main():
+  run_module()
+
+
+if __name__ == '__main__':
+  main()
